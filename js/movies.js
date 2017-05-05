891149c04f7fc9593475e37e68de807cd2c8eade
Sorebit <cmbk90@gmail.com>
2017-05-05 16:37:40 +0200

    Add movie list and lookup-er

---
+++ js/movies.js
@@ -0,0 +1,17 @@
+let filmweb = false;
+
+$('li').on('click', function() {
+	const name = $(this).find('.movie-name')[0];
+	const year = $(this).find('.movie-year')[0];
+	if(!name || !year)
+		return;
+	const full = $(name).text() + ' ' + $(year).text();
+	if(filmweb) {
+		const esc = escape($(name).text());
+		window.open('https://www.filmweb.pl/search?q=' + esc);
+		
+	} else {
+		const esc = escape(full);
+		window.open('https://www.google.pl/search?q=' + esc);
+	}
+});
