a09cca445c35dfffc7759cd7992fea0ab18e4021
Sorebit <cmbk90@gmail.com>
2017-05-09 18:31:36 +0200

    Start using jekyll

--- js/loader.js
+++
@@ -1,6 +0,0 @@
-$('#navbar-loader').load('https://sorebit.github.io/views/navbar.html', function(response, status, xhr) {
-  if(status == 'error') {
-    var msg = 'Sorry but there was an error: ';
-    console.error(msg + xhr.status + ' ' + xhr.statusText);
-  }
-});
