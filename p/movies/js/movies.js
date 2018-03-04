61c9d5a94423b0534155125c3159193d06884c67
Sorebit <cmbk90@gmail.com>
2018-03-04 09:50:32 +0100

    Add storing settings to movies; fix favicon

--- p/movies/js/movies.js
+++ p/movies/js/movies.js
@@ -9,9 +9,18 @@ $('li').on('click', function() {
 	if(filmweb) {
 		const esc = escape($(name).text());
 		window.open('https://www.filmweb.pl/search?q=' + esc);
-		
+		localStorage['useFilmweb'] = 'yes';
 	} else {
 		const esc = escape(full);
 		window.open('https://www.google.pl/search?q=' + esc);
+		localStorage['useFilmweb'] = 'no';
 	}
+});
+
+$(document).ready(function() {
+	if(localStorage['useFilmweb'] !== 'yes' && localStorage['useFilmweb'] !== 'no') {
+		localStorage['useFilmweb'] = 'yes';
+	}
+	const f = (localStorage['useFilmweb'] === 'yes');
+	$('input').prop('checked', f);
 });
